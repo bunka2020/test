a73417e6074ccd3c91d711ef769fb3bc550ccb5a
bunka2020 <dubovskiibogdan@gmail.com>
2020-01-20 15:08:08 +0300

    сделал if 5

---
+++ if 5.cpp
@@ -0,0 +1,40 @@
+#include <oistream>
+using namespase std;
+
+int main()
+
+{
+    int num1,num2,num3,otr = 0,pol = 0
+
+    cin>>num1>>num2>>num3;
+    if(num < 0)
+    {
+        otr++;
+    }
+    esle
+    {
+        pol++;
+    }
+    if(num2 < 0)
+    {
+        otr++;
+    }
+    esle
+    {
+        pol++;
+    }
+    if(num3 < 0)
+    {
+        otr++;
+
+    }
+    esle
+    {
+        pol++;
+    }
+    cout<<otr <<endl <<pol;
+    return 0;
+
+
+
+}
