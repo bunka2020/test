9d8896ebe7f464ef5aac10ceaf920808f0e676c9
bunka2020 <dubovskiibogda@gmail.com>
2020-01-22 15:45:26 +0300

    Решил Array1

---
+++ array 1/array 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>();
+
+            for (int i = 1; i <= N; i += 2) {
+                list.Add(i);
+            }
+
+            foreach (object vaule in list)
+            {
+                Console.WriteLine(vaule);
+            }
+        }
+    }
+}
