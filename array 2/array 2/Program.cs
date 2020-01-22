69d620c3d2f445044b4d355395915a69ae118003
bunka2020 <dubovskiibogda@gmail.com>
2020-01-22 16:04:56 +0300

    рушил array 2

---
+++ array 2/array 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic; 
+namespace array_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <=N; i *= 2)
+            {
+                numbers.Add(i);
+            }
+            foreach (int vaule in numbers)
+            {
+                Console.WriteLine(vaule);
+            }
+        }
+    }
+}
